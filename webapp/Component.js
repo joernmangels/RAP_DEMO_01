b06cfd3f2ec862ecfb42b83b7f3fc0e4639c324f
joernmangels <joern@mangels-mail.de>
2023-03-06 10:08:28 +0000

    0.0.1

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("de.varelmann.rapjm1.zrapdemojm1.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
